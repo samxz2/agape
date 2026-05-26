c4b949461c4578892a6d459b875e582304d032eb
samxz2 <sam14tv14@gmail.com>
2026-05-26 19:24:36 -0400

    hhola

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,2 +1,2 @@
-export const TASA_CAMBIO = Number(import.meta.env.PUBLIC_TASA_CAMBIO) || 0
+export const TASA_CAMBIO = Number(import.meta.env.PUBLIC_TASA_CAMBIO) || 720
 export const WHATSAPP_NUMERO = import.meta.env.PUBLIC_WHATSAPP_NUMERO || '584127050149'
